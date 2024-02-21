543b583264956bfe0eb03adfdfbabfcc79a2362e
GaurishN <gaurish.naik@raweng.com>
2024-02-21 10:33:51 +0530

    v5 upgrade

--- controller/prodcut.js
+++ controller/prodcut.js
@@ -5,7 +5,7 @@ const KeyValue = ormDB.key_value_table;
 async function getTotalVisitorsToday(req, res) {
   //getting visitors count data from SQL DB
   try {
-    const visitors = await KeyValue.getValuefor("view_count");
+    const visitors = await KeyValue.findOne({ where: { name: 'view_count' } });
     console.log({ type: "Visitors today", count: visitors.value })
     return res.send({ type: "Visitors today", count: visitors.value });
   } catch (e) {
